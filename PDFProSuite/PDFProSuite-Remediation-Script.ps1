bb5379b2529f082e33305d006d13e3b79b5900b3
Necaras2k <161664782+Necaras2k@users.noreply.github.com>
2026-02-12 13:31:36 -0800

    Refactor PDF Pro Suite remediation script

--- PDFProSuite/PDFProSuite-Remediation-Script.ps1
+++ PDFProSuite/PDFProSuite-Remediation-Script.ps1
@@ -1,3 +1,5 @@
+$tracker = 0
+
 $procList = @("pdfprosuite")
 foreach ($proc in $procList) {
     $process = Get-Process -Name $proc -ErrorAction SilentlyContinue
@@ -6,8 +8,10 @@ foreach ($proc in $procList) {
         Start-Sleep -Seconds 2
         if ($process) {
             Write-Host "Failed to stop PDF Pro Suite process => $process"
+            $tracker++
         } else {
             Write-Host "Stopped PDF Pro Suite process => $process"
+            $tracker++
         }
     }
 }
@@ -22,12 +26,13 @@ foreach ($user in $user_list) {
                 Remove-Item $installer -Force -ErrorAction SilentlyContinue
                 if (Test-Path $installer) {
                     Write-Host "Failed to remove PDF Pro Suite installer => $installer"
+                    $tracker++
                 } else {
                     Write-Host "Removed PDF Pro Suite installer => $installer"
+                    $tracker++
                 }
             }
         }
-
         $shortcuts = @(
             "C:\Users\$user\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\pdf pro suite.lnk",
             "C:\Users\$user\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\PDFProSuite\pdf pro suite.lnk",
@@ -38,12 +43,13 @@ foreach ($user in $user_list) {
                 Remove-Item $shortcut -ErrorAction SilentlyContinue
                 if (Test-Path $shortcut) {
                     Write-Host "Failed to remove PDF Pro Suite shortcut => $shortcut"
+                    $tracker++
                 } else {
                     Write-Host "Removed PDF Pro Suite shortcut => $shortcut"
+                    $tracker++
                 }
             }
         }
-
         $localPaths = @(
             "C:\Users\$user\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\PDFProSuite",
             "C:\Users\$user\AppData\Local\PDFProSuite",
@@ -54,19 +60,17 @@ foreach ($user in $user_list) {
                 Remove-Item $localPath -Recurse -Force -ErrorAction SilentlyContinue
                 if (Test-Path $localPath) {
                     Write-Host "Failed to remove PDF Pro Suite user path => $localPath"
+                    $tracker++
                 } else {
                     Write-Host "Removed PDF Pro Suite user path => $localPath"
+                    $tracker++
                 }
             }
         }
     }
 }
 
-
-$sid_list = Get-Item -Path "Registry::HKU\S-*" |
-    Select-String -Pattern "S-\d-(?:\d+-){5,14}\d+" |
-    ForEach-Object { $_.ToString().Trim() }
-
+$sid_list = Get-Item -Path "Registry::HKU\S-*" | Select-String -Pattern "S-\d-(?:\d+-){5,14}\d+" | ForEach-Object { $_.ToString().Trim() }
 foreach ($sid in $sid_list) {
     if ($sid -notlike "*_Classes*") {
         $runKeys = @("PDFProSuite")
@@ -76,12 +80,13 @@ foreach ($sid in $sid_list) {
                 Remove-ItemProperty -Path $runPath -Name $key -ErrorAction SilentlyContinue
                 if (Get-ItemProperty -Path $runPath -Name $key -ErrorAction SilentlyContinue) {
                     Write-Host "Failed to remove PDF Pro Suite HKU key => $runPath.$key"
+                    $tracker++
                 } else {
                     Write-Host "Removed PDF Pro Suite HKU key => $runPath.$key"
+                    $tracker++
                 }
             }
         }
-        
         $regPaths = @(
             "Registry::$sid\Software\PDF Pro Suite"
         )
@@ -90,10 +95,16 @@ foreach ($sid in $sid_list) {
                 Remove-Item $path -Recurse -Force -ErrorAction SilentlyContinue
                 if (Test-Path $path) {
                     Write-Host "Failed to remove PDF Pro Suite HKU key => $path"
+                    $tracker++
                 } else {
                     Write-Host "Removed PDF Pro Suite HKU key => $path"
+                    $tracker++
                 }
             }
         }
     }
+}
+
+if ($tracker -eq 0) {
+    Write-Host "Nothing found to remediate'
 }
