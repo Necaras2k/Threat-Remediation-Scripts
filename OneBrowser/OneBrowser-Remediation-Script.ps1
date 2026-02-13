22e488609cc14415101a59bc561140dd113376b1
Necaras2k <161664782+Necaras2k@users.noreply.github.com>
2026-02-13 12:59:37 -0800

    Update OneBrowser-Remediation-Script.ps1

--- OneBrowser/OneBrowser-Remediation-Script.ps1
+++ OneBrowser/OneBrowser-Remediation-Script.ps1
@@ -5,7 +5,7 @@ foreach ($proc in $procList) {
     $process = Get-Process -Name $proc -ErrorAction SilentlyContinue
     if ($process) {
         $process | Stop-Process -Force -ErrorAction SilentlyContinue
-        Start-Sleep -Seconds 2
+        Start-Sleep -Seconds 5
         if ($process) {
             Write-Host "Failed to stop OneBrowser proccess => $process"
             $tracker++
@@ -15,7 +15,7 @@ foreach ($proc in $procList) {
         }
     }
 }
-Start-Sleep -Seconds 2
+Start-Sleep -Seconds 5
 
 $user_list = Get-Item C:\users\* | Select-Object Name -ExpandProperty Name
 foreach ($user in $user_list) {
