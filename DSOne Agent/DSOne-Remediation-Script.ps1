bdff2a16cdf5ef6695c559bbe14ee3dfb3b39113
Necaras2k <161664782+Necaras2k@users.noreply.github.com>
2026-05-04 13:18:40 -0700

    Enhance DSOne remediation script functionality
    Refactor DSOne remediation script to improve process handling and cleanup logic.

--- DSOne Agent/DSOne-Remediation-Script.ps1
+++ DSOne Agent/DSOne-Remediation-Script.ps1
@@ -1,29 +1,83 @@
-Get-Process DSOne -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
-Get-Process DSOneWD -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
-sleep 2
+$tracker = 0
 
-rm "C:\windows\system32\tasks\DSOne Agent" -Force -Recurse -ErrorAction SilentlyContinue -ErrorVariable DirectoryError
-rm "C:\Program Files (x86)\Driver Support One" -Force -Recurse -ErrorAction SilentlyContinue -ErrorVariable DirectoryError
-Remove-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\TREE\DSOne Agent" -Recurse -ErrorAction SilentlyContinue
+$procList = @("DSOne", "DSOneWD", "DSOneWeb", "DSOneWebWD")
+foreach ($proc in $procList) {
+    $process = Get-Process -Name $proc -ErrorAction SilentlyContinue
+    if ($process) {
+        $process | Stop-Process -Force -ErrorAction SilentlyContinue
+        Start-Sleep -Seconds 5
+        $process = Get-Process -Name $proc -ErrorAction SilentlyContinue
+        if ($process) {
+            Write-Host "Failed to stop DSOne Agent process => $process"
+            $tracker++
+        } else {
+            Write-Host "Stopped DSOne Agent process => $process"
+            $tracker++
+        }
+    }
+}
+Start-Sleep -Seconds 5
 
-$user_list = Get-Item C:\users\* | Select-Object Name -ExpandProperty Name
-foreach ($i in $user_list) {
-    if ($i -notlike "*Public*") {
-        rm "C:\users\$i\downloads\DSOne*.exe" -Force -ErrorAction SilentlyContinue -ErrorVariable DirectoryError	
+$user_list = Get-Item C:\Users\* | Select-Object -ExpandProperty Name
+foreach ($username in $user_list) {
+    if ($username -notlike "*Public*") {
+        $targets = @(
+            "C:\Users\$username\downloads\DSOne*.exe",
+            "C:\Users\$username\downloads\DriverUpdate.exe"
+        )
+        foreach ($target in $targets) {
+            if (Test-Path -Path $target) {
+                Remove-Item $target -Force -Recurse -ErrorAction SilentlyContinue
+                if (Test-Path -Path $target) {
+                    Write-Host "Failed to remove DSOne Agent installer => $target"
+                    $tracker++
+                } else {
+                    Write-Host "Removed DSOne Agent installer => $target"
+                    $tracker++
+                }
+            }
+        }
     }
 }
 
-$result = test-path -Path "C:\windows\system32\tasks\DSOne Agent"
-if ($result -eq "True") {
-	"DSOne Agent wasn't removed => on C:\windows\system32\tasks\DSOne Agent"
+$paths = @(
+    "C:\Program Files (x86)\Driver Support One"
+)
+foreach ($path in $paths) {
+    if (Test-Path -Path $path) {
+        Remove-Item -Path $path -Force -Recurse -ErrorAction SilentlyContinue
+        if (Test-Path -Path $path) {
+            Write-Host "Failed to remove DSOne Agent system path => $path"
+            $tracker++
+        } else {
+            Write-Host "Removed DSOne Agent system path => $path"
+            $tracker++
+        }
+    }
 }
 
-$result = test-path -Path "C:\Program Files (x86)\Driver Support One"
-if ($result -eq "True") {
-	"DSOne Agent wasn't removed => on C:\Program Files (x86)\Driver Support One"
+$tasks = Get-ScheduledTask -TaskName *DSOne* -ErrorAction SilentlyContinue | Select-Object -ExpandProperty TaskName
+foreach ($task in $tasks) {
+    Unregister-ScheduledTask -TaskName $task -Confirm:$false -ErrorAction SilentlyContinue
 }
 
-$result = test-path -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\TREE\DSOne Agent"
-if ($result -eq "True") {
-	"DSOne Agent wasn't removed => on HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\TREE\DSOne Agent"
+$taskPaths = @(
+    "C:\windows\system32\tasks\DSOne Agent",
+    "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\TREE\DSOne Agent"
+)
+foreach ($taskPath in $taskPaths) {
+    if (Test-Path -Path $taskPath) {
+        Remove-Item $taskPath -Recurse -Force -ErrorAction SilentlyContinue
+        if (Test-Path -Path $taskPath) {
+            Write-Host "Failed to remove DSOne Agent task => $taskPath"
+            $tracker++
+        } else {
+            Write-Host "Removed DSOne Agent task => $taskPath"
+            $tracker++
+        }
+    }
+}
+
+if ($tracker -eq 0) {
+    Write-Host "Nothing found to remediate"
 }
